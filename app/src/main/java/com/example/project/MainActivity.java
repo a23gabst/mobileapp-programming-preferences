8694365279fd86e80eff105e9e1a764defb1ecfc
a23gabst <a23gabst@student.his.se>
2024-04-26 17:04:07 +0200

    Får det som skrivs i EditText att sparas och uppdatera

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,22 +1,26 @@
 package com.example.project;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button minBtn = findViewById(R.id.prefButton);
+        Button minBtn = findViewById(R.id.prefButtons);
 
         minBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        myPreferenceRef = getSharedPreferences("MyPreferenceName", MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
+
+        // Display preferences
+        TextView prefTextRef=new TextView(this);
+        prefTextRef=(TextView)findViewById(R.id.prefText);
+        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
+
     }
 
 }
